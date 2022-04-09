f8316ed621a96508d65a73e5230107dd8d8c07ed
Reven Ferlian Erlangga <reven.erlangga@gmail.com>
2022-04-09 09:56:02 +0700

    add: campaign & campaign image model

---
+++ Server/models/campaign-image.go
@@ -0,0 +1,12 @@
+package models
+
+import "time"
+
+type CampaignImage struct {
+	ID         int
+	CampaignID int
+	FileName   string
+	IsPrimary  int
+	CreatedAt  time.Time
+	UpdatedAt  time.Time
+}
--- Server/models/campaign.go
+++ Server/models/campaign.go
@@ -1,1 +1,18 @@
 package models
+
+import "time"
+
+type Campaign struct {
+	ID               int
+	UserID           int
+	Name             string
+	ShortDescription string
+	Description      string
+	Perks            string
+	BackerCount      int
+	GoalAmount       int
+	CurrentMounth    int
+	Slug             string
+	CreatedAt        time.Time
+	UpdatedAt        time.Time
+}
